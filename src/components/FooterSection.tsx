c1281eb98e39c892be5276acd277c9186141ed92
Monikar <138695117+monikadei@users.noreply.github.com>
2025-07-17 16:44:44 +0530

    footer page fix

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -6,12 +6,12 @@ const FooterSection = () => {
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-8">
           <div className="col-span-2">
             <div className="flex items-center">
-              <span className="text-white font-extrabold text-xl">Rhino Review</span>
+              <span className="text-white font-extrabold text-xl"><a href="https://rhino-review.rhinosoft.in/">Rhino Review</a></span>
             </div>
             <p className="mt-4 text-gray-300 text-sm">
               Rhino Review helps businesses collect, manage and showcase their customer reviews to improve their online reputation and attract more customers.
             </p>
-            <div className="mt-6 flex space-x-4">
+            {/* <div className="mt-6 flex space-x-4">
               <a href="#" className="text-gray-400 hover:text-white">
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
@@ -37,7 +37,7 @@ const FooterSection = () => {
                   <path fillRule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z" clipRule="evenodd" />
                 </svg>
               </a>
-            </div>
+            </div> */}
           </div>
           
           <div>
@@ -102,7 +102,10 @@ const FooterSection = () => {
             <a href="/cookies" className="text-gray-400 hover:text-white text-sm">Cookie Policy</a>
           </div>
           <p className="mt-8 md:mt-0 text-base text-gray-400 md:order-1">
-            &copy; {new Date().getFullYear()} Rhino Review. All rights reserved.
+            &copy; 
+            {/* {new Date().getFullYear()}  */}
+            2025 
+            powered by @<a href="https://rhinosoft.in/" target="_blank">Rhinosoft.in</a> / All rights reserved.
           </p>
         </div>
       </div>
